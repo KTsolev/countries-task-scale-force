6304aed68528803621690cf99184dfbe50e499d2
Konstantin Tsolev <ktsolev@gmail.com>
2021-12-02 16:08:05 +0200

    config headers

--- src/network/api.js
+++ src/network/api.js
@@ -2,5 +2,10 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
     baseURL:'https://api.worldbank.org/v2/',
+    withCredentials: false,
+    headers: {
+      'Access-Control-Allow-Origin' : '*',
+      'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',   
+    }
 });
 
